19401aa9d0ae1919f370358875927d8590b6dffc
NxthxnX <ozerov.ka@phystech.edu>
2025-05-19 16:32:02 +0300

    Create A. Air conditioner.cpp

---
+++ 1. Complexity, testing, and special cases/A. Air conditioner.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+	int troom, tcond;
+	std::string mode;
+	std::cin >> troom >> tcond >> mode;
+	if (mode == "freeze") {
+		std::cout << std::min(troom, tcond) << std::endl;
+	}
+	else if (mode == "heat") {
+		std::cout << std::max(troom, tcond) << std::endl;
+	}
+	else if (mode == "auto") {
+		std::cout << tcond << std::endl;
+	}
+	else if (mode == "fan") {
+		std::cout << troom << std::endl;
+	}
+	else {
+		std::cout << "Unknown error" << std::endl;
+	}
+	return 0;
+}
